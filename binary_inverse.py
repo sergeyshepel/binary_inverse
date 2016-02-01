5ac247a82343563b1788061ea981814a6f6816b4
Sergey <sergey.shepel@outlook.com>
2016-02-01 21:55:35 +0000

    Add comments and input ability

--- binary_inverse.py
+++ binary_inverse.py
@@ -1,6 +1,14 @@
-input_data = [1, 0, 0, 1]
+def get_initial_data():
+    """ Get initial data """
+    input_string = ''
+    while not input_string:
+        input_string = raw_input('Enter sequence of digits separated by comma: ')
+    input_data = map(int, input_string.split(','))
+    print "input data: %s" % input_data
+    return input_data
 
 def get_decimal_represent(binary_represent):
+    """ Convert initial data into decimal representation"""
     decimal_represent = 0
     for i, digit in enumerate(binary_represent):
         decimal_represent = decimal_represent + digit * pow(-2, i)
@@ -8,11 +16,13 @@ def get_decimal_represent(binary_represent):
     return decimal_represent
 
 def invert_decimal_represent(decimal_represent):
+    """ Invert decimal representation """
     inverted_decimal = decimal_represent * -1
     print "inverted value = %s" % (inverted_decimal)
     return inverted_decimal
 
 def get_the_shortest_represent(inverted_decimal):
+    """ Convert inverted decimal representation into negabinary """
     digits = []
     if not inverted_decimal:
         digits = ['0']
@@ -24,4 +34,11 @@ def get_the_shortest_represent(inverted_decimal):
             digits.append(str(remainder))
     print "output = %s" % map(int, digits)
 
-get_the_shortest_represent(invert_decimal_represent(get_decimal_represent(input_data)))
+# main
+get_the_shortest_represent(
+    invert_decimal_represent(
+        get_decimal_represent(
+            get_initial_data()
+        )
+    )
+)
