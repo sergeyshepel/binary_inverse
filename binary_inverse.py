c84c453b774b0fb15e83abf1c959a893b242d9ed
Sergey <sergey.shepel@outlook.com>
2016-02-02 09:32:50 +0000

    Combine in one function

--- binary_inverse.py
+++ binary_inverse.py
@@ -7,22 +7,16 @@ def get_initial_data():
     print "input data: %s" % input_data
     return input_data
 
-def get_decimal_represent(binary_represent):
-    """ Convert initial data into decimal representation"""
+def binary_inverse(initial_data):
+    # Convert initial data into decimal representation
     decimal_represent = 0
-    for i, digit in enumerate(binary_represent):
+    for i, digit in enumerate(initial_data):
         decimal_represent = decimal_represent + digit * pow(-2, i)
     print "decimal value = %s" % decimal_represent
-    return decimal_represent
-
-def invert_decimal_represent(decimal_represent):
-    """ Invert decimal representation """
+    # Invert decimal representation
     inverted_decimal = decimal_represent * -1
     print "inverted value = %s" % (inverted_decimal)
-    return inverted_decimal
-
-def get_the_shortest_represent(inverted_decimal):
-    """ Convert inverted decimal representation into negabinary """
+    # Convert inverted decimal representation into negabinary
     digits = []
     if not inverted_decimal:
         digits = ['0']
@@ -33,12 +27,7 @@ def get_the_shortest_represent(inverted_decimal):
                 inverted_decimal, remainder = inverted_decimal + 1, remainder + 2
             digits.append(str(remainder))
     print "output = %s" % map(int, digits)
+    return map(int, digits)
 
 # main
-get_the_shortest_represent(
-    invert_decimal_represent(
-        get_decimal_represent(
-            get_initial_data()
-        )
-    )
-)
+binary_inverse(get_initial_data())
